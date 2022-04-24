128a64c26436e0f3450ba24cea03f97b5743865a
BenHurJF <beiujeffer@hotmail.com>
2022-04-24 05:36:05 -0300

    Add gitClone project

---
+++ cypress/support/cli_commands.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+Cypress.Commands.add('cloneViaSSH', project => {
+    const domain = Cypress.config('baseUrl').replace('http://', '').replace('/', '')
+  
+    
+    cy.exec(`cd temp/ && git clone git@localhost:root/${project.name}.git`)
+    // cy.exec(`cd temp/ && git clone git@${domain}:${Cypress.env('user_name')}/${project.name}.git`)
+
+  })
+  
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './api_commands'
+import './cli_commands'
 import './gui_commands'
+
 
 
 require('cypress-xpath');
